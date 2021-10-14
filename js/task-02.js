3e7c82eaf28c50ed39ae173095a4ad25ea2a38e7
Dmitriy Dudnik <wed2288@gmail.com>
2021-10-15 00:02:18 +0300

    добавляет JS для второй задачи

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,13 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ingredientsContainerEl = document.querySelector('#ingredients');
+
+const elements = ingredients.map(element => {
+  const listsEl = document.createElement('li');
+  listsEl.textContent = element;
+  listsEl.classList.add('item');
+
+  return listsEl;
+});
+
+ingredientsContainerEl.append(...elements);
