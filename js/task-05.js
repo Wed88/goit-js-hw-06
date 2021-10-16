e2092ac880f7d0a8ff84964bdd545e9bc63cc336
Dmitriy Dudnik <wed2288@gmail.com>
2021-10-16 23:19:58 +0300

    add JS 5 task

---
+++ js/task-05.js
@@ -0,0 +1,8 @@
+const inputEl = document.querySelector('#name-input');
+const outputEl = document.querySelector('#name-output');
+
+inputEl.addEventListener('input', onInputChenge);
+
+function onInputChenge(event) {
+  outputEl.textContent = event.currentTarget.value;
+}
