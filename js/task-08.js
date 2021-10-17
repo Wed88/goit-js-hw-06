ec1a00844ed214b1263353f49360e3c568d0d79b
Dmitriy Dudnik <wed2288@gmail.com>
2021-10-17 22:22:24 +0300

    add JS 8 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (
+    event.currentTarget.elements.email.value === '' ||
+    event.currentTarget.elements.password.value === ''
+  ) {
+    return console.log('Внимание! Все поля должны быть заполнены');
+  }
+
+  console.log(`${email.name}: ${email.value}, ${password.name}: ${password.value}`);
+
+  event.currentTarget.reset();
+}
